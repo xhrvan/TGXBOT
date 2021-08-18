e1813bee7f8af78b23c4935f1ed37b34ed27e707
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-18 20:36:24 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -4,30 +4,28 @@ import random
 from colorama import Fore, Style, Back
 
 
-aura = """
-╔╗──╔═══╦═══╦═══╦═╗─╔╦═══╦═══╦═══╦╗──╔╗
-║║──║╔══╣╔═╗║╔══╣║╚╗║╠╗╔╗║╔═╗║╔═╗║╚╗╔╝║  
-║║──║╚══╣║─╚╣╚══╣╔╗╚╝║║║║║║─║║╚═╝╠╗╚╝╔╝  
-║║─╔╣╔══╣║╔═╣╔══╣║╚╗║║║║║║╚═╝║╔╗╔╝╚╗╔╝   
-║╚═╝║╚══╣╚╩═║╚══╣║─║║╠╝╚╝║╔═╗║║║╚╗─║║   
-╚═══╩═══╩═══╩═══╩╝─╚═╩═══╩╝─╚╩╝╚═╝─╚╝   
+tgx = """
+╔══╗╔══╗╔╗╔╗╔══╗╔═╗╔══╗
+╚╗╔╝║╔═╣╚╗╔╝║╔╗║║║║╚╗╔╝
+─║║─║╚╗║╔╝╚╗║╔╗║║║║─║║─
+─╚╝─╚══╝╚╝╚╝╚══╝╚═╝─╚╝─
 """
 logo = """
 
 """
 bhai_bolte = """
-#Legendary LEGENDBOT          
-Made With Love By Team LEGENDBOT
+#Legendary TGXBOT          
+Made With Love By Team TGXBOT
 """
                                                                                                             
 print("")
-print(Style.BRIGHT + Fore.MAGENTA + aura)
+print(Style.BRIGHT + Fore.MAGENTA + tgx)
 print(Style.RESET_ALL)
 print(Style.BRIGHT + Fore.BLUE + logo)
 print(Style.RESET_ALL)
 print(Style.BRIGHT + Fore.CYAN + Back.BLUE + bhai_bolte)
 print(Style.RESET_ALL)
-print("""Welcome To LEGENDBOT String Generator By @LEGENDOwner""")
+print("""Welcome To LEGENDBOT String Generator By @TGXBOTZ""")
 print("""Kindly Enter Your Details To Continue ! """)
 
 API_KEY = input("API_KEY: ")
@@ -41,11 +39,11 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @LEGEND_Userbot For Any Help !",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @TGXBOTZ_CHAT For Any Help !",
             )
 
             print(
-                "Thanks for Choosing LEGENDBOT Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing TGXBOT Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
