c70a2477f07c47bb9800eefcb0352446adc0551a
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-18 20:37:24 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -2,7 +2,7 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print("")
-print("""Welcome To LEGENDBOT String Session\nGenerator By @LEGENDOwner\n\n""")
+print("""Welcome To TGXBOT String Session\nGenerator By @TGXBOTZ\n\n""")
 print("""Enter Your Valid Details To Continue!\n\n """)
 
 API_KEY = input("API_ID:  ")
@@ -18,11 +18,11 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @LEGENDSupport For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @TGXBOTZ_CHAT For Any Help!\n\n",
             )
 
             print(
-                "Thanks for Choosing LEGENDBOT Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing TGXBOT Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
